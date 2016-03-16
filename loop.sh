a8e8df1e94b04b7def2c2ba1baaaf0ef132fecd4
Qian Hong <qhong@codeweavers.com>
2016-03-16 12:46:45 +0800

    Use normal shell.

--- loop.sh
+++ loop.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 rm -f all.txt
-for i in {0..8}
+echo 111111111111111111111111111111111111111111111111111111111111111111111111111 > long.txt
+for i in {0..7}
 do
     ./grow.sh | tee -a all.txt
     echo $i
